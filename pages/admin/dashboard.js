bc0073712520ab8456364cfe4a9c1982bbfe8c27
pathwalkingtechnology <pathwalkingtechnology@gmail.com>
2024-10-18 10:22:55 -0300

    V.2.0.3

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,11 +1,21 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Dashboard() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const admin = localStorage.getItem('admin');
+    if (!admin) {
+      router.push('/admin');
+    }
+  }, []);
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <ul>
-        <li><Link href="/admin/productos">Gestionar Productos</Link></li>
+        <li><Link href="/admin/productos">Gestionar Productos</Link><br/></li>
         <li><Link href="/admin/categorias">Gestionar Categorías</Link></li>
       </ul>
     </div>
